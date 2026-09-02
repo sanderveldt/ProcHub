e0cca1a2948f1b5ab47e7f8a734cc2947a6b805e
Sander <282338706+sanderveldt@users.noreply.github.com>
2026-09-02 23:55:41 +0200

    Application NotFoundException added

---
+++ src/ProcHub.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,9 @@
+namespace ProcHub.Application.Exceptions;
+
+public sealed class NotFoundException : Exception
+{
+    public NotFoundException(string message)
+        : base(message)
+    {
+    }
+}
