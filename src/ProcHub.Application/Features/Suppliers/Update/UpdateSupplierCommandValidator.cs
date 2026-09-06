fef188492e59a0ed6dbd963286b5a68bd1e4a3de
Sander <282338706+sanderveldt@users.noreply.github.com>
2026-09-06 20:06:48 +0200

    UpdateSupplierCommand, validator and handler setup

---
+++ src/ProcHub.Application/Features/Suppliers/Update/UpdateSupplierCommandValidator.cs
@@ -0,0 +1,67 @@
+using FluentValidation;
+using ProcHub.Domain.Suppliers;
+
+namespace ProcHub.Application.Features.Suppliers.Update;
+
+public sealed class UpdateSupplierCommandValidator
+    : AbstractValidator<UpdateSupplierCommand>
+{
+    public UpdateSupplierCommandValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .WithMessage("Supplier name is required.")
+            .MaximumLength(SupplierConstants.NameMaxLength)
+            .WithMessage($"Supplier name cannot exceed {SupplierConstants.NameMaxLength} characters.");
+
+        RuleFor(x => x.DefaultPaymentTermId)
+            .NotEmpty()
+            .GreaterThan(0)
+            .WithMessage("Default payment term is required.");
+
+        RuleFor(x => x.FullName)
+            .MaximumLength(SupplierConstants.FullNameMaxLength)
+            .WithMessage($"Supplier full name cannot exceed {SupplierConstants.FullNameMaxLength} characters.");
+    
+        RuleFor(x => x.MainShippingTermId)
+            .GreaterThan(0)
+            .When(x => x.MainShippingTermId.HasValue);
+
+        RuleFor(x => x.SecondaryShippingTermId)
+            .GreaterThan(0)
+            .When(x => x.SecondaryPaymentTermId.HasValue);
+
+        RuleFor(x => x.SampleShippingTermId)
+            .GreaterThan(0)
+            .When(x => x.SampleShippingTermId.HasValue);
+
+        RuleFor(x => x.SecondaryPaymentTermId)
+            .GreaterThan(0)
+            .When(x => x.SecondaryPaymentTermId.HasValue);
+        
+        RuleFor(x => x.ShippingTimeDays)
+            .GreaterThanOrEqualTo(0)
+            .When(x => x.ShippingTimeDays.HasValue)
+            .WithMessage("Shipping days can't be negative.");
+
+        RuleFor(x => x.ProductionTimeDays)
+            .GreaterThanOrEqualTo(0)
+            .When(x => x.ProductionTimeDays.HasValue)
+            .WithMessage("Production days can't be negative.");
+        
+        RuleFor(x => x.MainLeadTimeDays)
+            .GreaterThanOrEqualTo(0)
+            .When(x => x.MainLeadTimeDays.HasValue)
+            .WithMessage("Main lead time can't be negative.");
+        
+        RuleFor(x => x.SecondaryLeadTimeDays)
+            .GreaterThanOrEqualTo(0)
+            .When(x => x.SecondaryLeadTimeDays.HasValue)
+            .WithMessage("Secondary lead time can't be negative.");
+
+        RuleFor(x => x.SampleLeadTimeDays)
+            .GreaterThanOrEqualTo(0)
+            .When(x => x.SampleLeadTimeDays.HasValue)
+            .WithMessage("Sample lead time can't be negative.");
+    }
+}
---
+++ src/ProcHub.Application/Features/Suppliers/Update/UpdateSupplierHandler.cs
@@ -0,0 +1,141 @@
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using ProcHub.Application.Abstractions;
+using ProcHub.Application.Exceptions;
+
+namespace ProcHub.Application.Features.Suppliers.Update;
+
+public sealed class UpdateSupplierHandler(
+    IProcHubDbContext dbContext,
+    IValidator<UpdateSupplierCommand> validator)
+{
+    public async Task<SupplierResult> HandleAsync(
+        string code,
+        UpdateSupplierCommand command,
+        CancellationToken cancellationToken = default)
+    {
+        await validator.ValidateAndThrowAsync(
+            command,
+            cancellationToken: cancellationToken);
+        
+        var supplier = await dbContext.Suppliers
+            .FirstOrDefaultAsync(
+                s => s.Code == code,
+                cancellationToken)
+            ?? throw new NotFoundException(
+                $"Supplier with code {code} not found.");
+
+        var defaultPaymentTerm = await dbContext.PaymentTerms
+            .FindAsync(
+                [command.DefaultPaymentTermId],
+                cancellationToken)
+            ?? throw new NotFoundException(
+                $"PaymentTerm {command.DefaultPaymentTermId} not found");
+        
+        supplier.SetDefaultPaymentTerm(defaultPaymentTerm);
+        
+        supplier.SetName(command.Name);
+        supplier.SetFullName(command.FullName);
+
+
+        if (command.MainShippingTermId is null)
+        {
+            supplier.SetMainShippingTerm(null);
+        }
+        else
+        {
+            var mainShippingTerm = await dbContext.ShippingTerms
+                .FindAsync(
+                    [command.MainShippingTermId.Value],
+                    cancellationToken)
+                ?? throw new NotFoundException(
+                    $"ShippingTerm {command.MainShippingTermId.Value} not found.");
+            
+            supplier.SetMainShippingTerm(mainShippingTerm);
+        }
+
+        if (command.SecondaryShippingTermId is null)
+        {
+            supplier.SetSecondaryShippingTerm(null);
+        }
+        else
+        {
+            var secondaryShippingTerm = await dbContext.ShippingTerms
+                .FindAsync(
+                    [command.SecondaryShippingTermId.Value],
+                    cancellationToken)
+                ?? throw new NotFoundException(
+                    $"ShippingTerm {command.SecondaryShippingTermId.Value} not found.");
+                
+            supplier.SetSecondaryShippingTerm(secondaryShippingTerm);
+        }
+
+        if (command.SampleShippingTermId is null)
+        {
+            supplier.SetSampleShippingTerm(null);
+        }
+        else
+        {
+            var sampleShippingTerm = await dbContext.ShippingTerms
+                .FindAsync(
+                    [command.SampleShippingTermId.Value],
+                    cancellationToken)
+                ?? throw new NotFoundException(
+                    $"ShippingTerm {command.SampleShippingTermId.Value} not found.");
+                
+            supplier.SetSampleShippingTerm(sampleShippingTerm);
+        }
+
+        if (command.SecondaryPaymentTermId is null)
+        {
+            supplier.SetSecondaryPaymentTerm(null);
+        }
+        else
+        {
+            var secondaryPaymentTerm = await dbContext.PaymentTerms
+                .FindAsync(
+                    [command.SecondaryPaymentTermId.Value],
+                    cancellationToken)
+                ?? throw new NotFoundException(
+                    $"PaymentTerm {command.SecondaryPaymentTermId.Value} not found.");
+            
+            supplier.SetSecondaryPaymentTerm(secondaryPaymentTerm);
+        }
+
+        supplier.SetShippingTimeDays(command.ShippingTimeDays);
+        supplier.SetProductionTimeDays(command.ProductionTimeDays);
+        supplier.SetMainLeadTimeDays(command.MainLeadTimeDays);
+        supplier.SetSecondaryLeadTimeDays(command.SecondaryLeadTimeDays);
+        supplier.SetSampleLeadTimeDays(command.SampleLeadTimeDays);
+
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return new SupplierResult(
+            supplier.Code,
+            supplier.Name,
+
+            supplier.DefaultPaymentTermId,
+            defaultPaymentTerm.Description,
+
+            supplier.FullName,
+
+            supplier.MainShippingTermId,
+            supplier.MainShippingTerm?.Name,
+            supplier.SecondaryShippingTermId,
+            supplier.SecondaryShippingTerm?.Name,
+            supplier.SampleShippingTermId,
+            supplier.SampleShippingTerm?.Name,
+
+            supplier.SecondaryPaymentTermId,
+            supplier.SecondaryPaymentTerm?.Description,
+
+            supplier.ShippingTimeDays,
+            supplier.ProductionTimeDays,
+            supplier.MainLeadTimeDays,
+            supplier.SecondaryLeadTimeDays,
+            supplier.SampleLeadTimeDays,
+
+            supplier.Status,
+            supplier.CreationDate);
+    }
+}
